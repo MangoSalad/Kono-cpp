58a28498c0004836a1a8e94ce5b7d65cf6cf9dd3
mango <8378656+MangoSalad@users.noreply.github.com>
2018-01-22 15:42:59 -0500

    Included 7 minimum classes

--- src/board.h
+++ src/board.h
@@ -1,24 +1,8 @@
 #ifndef BOARD_H
 #define BOARD_H
 
-class board
-{
- public:
-  board( unsigned short inSize );
-  short place( unsigned short location );
-  void print();
-  unsigned short getSize() const { return size; }
-  bool filled() const { return filledCount == size * size; }
+class board {
 
- protected:
-
- private:
-  unsigned short size;
-  bool **table;
-  unsigned short filledCount;
-
-  // utility function
-  void drawLine( unsigned short size );
 };
 
 #endif
--- src/player.h
+++ src/player.h
@@ -1,27 +1,10 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-#include "board.h"
-#include <string>
-
-using namespace std;
-
-class player
-{
- public:
-  player();
-  virtual void play( board & gameboard );
-  unsigned short getPoints() const { return points; }
-  string getName() const { return name; }
-
-  void play2( board & gameboard );
-  virtual unsigned short getLocation( board & gameboard ) = 0;
-
- protected:
-  unsigned short points;
-  string name;
-
- private:
+class player {
+    public:
+        player();
+        virtual play();
 
 };
 
